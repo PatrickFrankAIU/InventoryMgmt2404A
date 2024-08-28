c90c2067d538b453b3629349f3def44f5fb95c33
Pat Frank <pfrank@aiufl.edu>
2024-08-28 13:56:41 -0400

    minor changes to variable names and added some comments (for clarity)

--- main.js
+++ main.js
@@ -20,17 +20,19 @@ const inventory = [
 
 // Function to display the inventory
 function displayInventory() {
-    const inventoryDisplay = document.getElementById('inventoryDisplay');
+    // inventoryDisplay contains all product groups (categories and items)
+    let inventoryDisplay = document.getElementById('inventoryDisplay'); 
     inventoryDisplay.innerHTML = ''; // Clear any existing content
 
     // Iterate through the inventory and create HTML elements to display the data
     inventory.forEach(category => {
-        const categoryEl = document.createElement('div');
-        categoryEl.innerHTML = "<strong>" + category.category + "<strong>";
+        // itemGroup contains ONE category and its item list
+        let itemGroup = document.createElement('div'); // categories and items
+        itemGroup.innerHTML = "<strong>" + category.category + "<strong>";
         category.products.forEach(product => {
-            categoryEl.innerHTML += "<div>" + product.product + ": " + product.quantity;
+            itemGroup.innerHTML += "<div>" + product.product + ": " + product.quantity;
         });
-        inventoryDisplay.appendChild(categoryEl);
+        inventoryDisplay.appendChild(itemGroup); // attaches the new product to the display
     });
 }
 
